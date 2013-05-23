4fcaa83340e36c31f75c40e207ec332975f40ba7
Thanh <lamphongthanh1992@gmail.com>
2013-05-23 18:59:53 +0700

    Website for Mobile
    Source code đầu tiên

---
+++ MobileStoreWebsite/MobileStoreBLL/MobileStoreModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Phong Thanh\documents\visual studio 2012\Projects\MobileStoreWebsite\MobileStoreBLL\MobileStoreModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ MobileStoreWebsite/MobileStoreBLL/ViewModel/ChiTietSanPham.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileStoreBLL.ViewModel
+{
+    public class ChiTietSanPham
+    {
+        public SanPham SanPham { get; set; }
+        public List<BinhLuan> DSBinhLuan { get; set; }
+    }
+}
